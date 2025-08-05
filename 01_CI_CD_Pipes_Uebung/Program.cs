61f72e5f598c70ad30484a993882d8c7601a10e5
Daniel Mezker <dmezker06@gmail.com>
2025-08-05 14:06:57 +0200

    little Change

--- 01_CI_CD_Pipes_Uebung/Program.cs
+++ 01_CI_CD_Pipes_Uebung/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gebe bitte deine erste Zahl ein, welche du addieren möchtest");
+            Console.WriteLine("Gebe bitte deine erste Zahl ein, welche du addieren möchtest!");
             int zahl1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Gebe bitte deine zweite Zahl ein, welche du addieren möchtest");
+            Console.WriteLine("Gebe bitte deine zweite Zahl ein, welche du addieren möchtest!");
             int zahl2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\n");
 
