e82d360b0cbba429b131c775cdc9e3ea90089712
Daniel Mezker <dmezker06@gmail.com>
2025-08-05 14:03:01 +0200

    Update Program.cs

--- 01_CI_CD_Pipes_Uebung/Program.cs
+++ 01_CI_CD_Pipes_Uebung/Program.cs
@@ -19,7 +19,7 @@
             Console.WriteLine("Berechnung beider Zahlen... \n");
 
             Thread.Sleep(3000);
-            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Das Ergbnis ist: " + sum + "\n");
         }
     }
