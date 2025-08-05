bdaa64b624c21eb7d3e9d53559eeca1cfcd73bbd
Daniel Mezker <dmezker06@gmail.com>
2025-08-05 11:01:24 +0200

    First Upload

---
+++ 01_CI_CD_Pipes_Uebung/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01_CI_CD_Pipes_Uebung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gebe bitte deine erste Zahl ein, welche du addieren möchtest");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Gebe bitte deine zweite Zahl ein, welche du addieren möchtest");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            int sum = zahl1 + zahl2;
+
+            Console.WriteLine("Das Ergbnis ist: " + sum);
+        }
+    }
+}
+
