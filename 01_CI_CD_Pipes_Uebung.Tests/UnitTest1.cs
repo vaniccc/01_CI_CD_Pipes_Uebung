d6d05d5e8f7f7e2794d83a7ef3914489a0471921
Daniel Mezker <dmezker06@gmail.com>
2025-08-05 16:22:33 +0200

    final ci-cd Test

---
+++ 01_CI_CD_Pipes_Uebung.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace _01_CI_CD_Pipes_Uebung.Tests;
+
+public class Tests
+{
+    public class RechnerTests
+    {
+        [Test]
+        public void Addiere_Sollte_Zahlen_Addieren()
+        {
+            int testNumber1 = 4;
+            int testNumber2 = 5;
+            int expectedResult = 9;
+
+            int Result = Rechner.Addiere(zahl1, zahl2);
+
+            Assert.AreEqual(erwartetesErgebnis, ergebnis);
+        }
+    }
+
+    public class Rechner
+    {
+        public static int Addiere(int a, int b) 
+        { 
+            return a + b; 
+        }
+    }
+}
--- 01_CI_CD_Pipes_Uebung/Program.cs
+++ 01_CI_CD_Pipes_Uebung/Program.cs
@@ -5,12 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Gebe bitte deine erste Zahl ein, welche du addieren möchtest!");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            int enteredNumber1 = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Gebe bitte deine zweite Zahl ein, welche du addieren möchtest!");
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            int enteredNumber1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\n");
 
-            int sum = zahl1 + zahl2;
+            int sum = Rechner.Addiese(zahl1, zahl2);
 
             Thread.Sleep(500);
             Console.WriteLine("Aufnahme beider Zahlen... \n");
