f85f8d60e38b79437c8a526b9693f24ac2263cc4
Daniel Mezker <dmezker06@gmail.com>
2025-08-06 10:00:35 +0200

    Final Upload

--- 01_CI_CD_Pipes_Uebung.Tests/RechnerTests.cs
+++ 01_CI_CD_Pipes_Uebung.Tests/RechnerTests.cs
@@ -1,37 +1,38 @@
 using NUnit.Framework;
 using _01_CI_CD_Pipes_Uebung;
 
-namespace _01_CI_CD_Pipes_Uebung.Tests;
-
-public class RechnerTests
+namespace _01_CI_CD_Pipes_Uebung.Tests
 {
-    [Test]
-    public void Test_Add()
+    public class RechnerTests
     {
-        Assert.AreEqual(9, Rechner.Addiere(4, 5));
-    }
+        [Test]
+        public void Test_Add()
+        {
+            Assert.AreEqual(9, Rechner.Addiere(4, 5));
+        }
 
-    [Test]
-    public void Test_Subtract()
-    {
-        Assert.AreEqual(2, Rechner.Subtrahiere(5, 3));
-    }
+        [Test]
+        public void Test_Subtract()
+        {
+            Assert.AreEqual(2, Rechner.Subtrahiere(5, 3));
+        }
 
-    [Test]
-    public void Test_Multiply()
-    {
-        Assert.AreEqual(15, Rechner.Multipliziere(3, 5));
-    }
+        [Test]
+        public void Test_Multiply()
+        {
+            Assert.AreEqual(15, Rechner.Multipliziere(3, 5));
+        }
 
-    [Test]
-    public void Test_Devide()
-    {
-        Assert.AreEqual(2, Rechner.Dividiere(10, 5));
-    }
+        [Test]
+        public void Test_Devide()
+        {
+            Assert.AreEqual(2, Rechner.Dividiere(10, 5));
+        }
 
-    [Test]
-    public void Test_DevideByZeroExceptionThrow()
-    {
-        Assert.Throws<DivideByZeroException>(() => Rechner.Dividiere(10, 0));
+        [Test]
+        public void Test_DevideByZeroExceptionThrow()
+        {
+            Assert.Throws<DivideByZeroException>(() => Rechner.Dividiere(10, 0));
+        }
     }
 }
